2bdd086440b21415d33ef92e77c5f18e5013d0a9
Ольга Бугрова <bugrovaoa@it2g.ru>
2020-10-17 12:33:36 +0400

    feat: add package winston and morgan, add save logs query, body, url

--- src/app.js
+++ src/app.js
@@ -5,13 +5,33 @@ const YAML = require('yamljs');
 const userRouter = require('./resources/users/user.router');
 const boardRouter = require('./resources/boards/board.router');
 const taskRouter = require('./resources/tasks/task.router');
+const fs = require('fs');
 
 const app = express();
 const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
 
+const morgan = require('morgan');
+
 app.use(express.json());
 
 app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
+morgan.token('body', res => {
+  const { body, params } = res;
+  return `BODY: ${JSON.stringify(body)}, QUERY PARAMETERS: ${JSON.stringify(
+    params
+  )}`;
+});
+
+app.use(morgan('METHOD: :method URL: :url :body :response-time :req[header]'));
+
+app.use(
+  morgan('METHOD: :method URL: :url :body :response-time :req[header]', {
+    stream: fs.createWriteStream(path.join(__dirname, './logs/access.log'), {
+      flags: 'a'
+    })
+  })
+);
 
 app.use('/', (req, res, next) => {
   if (req.originalUrl === '/') {
