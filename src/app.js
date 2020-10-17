09bb5a2bf37772164198dc1e6a9251dc158e1fa1
Ольга Бугрова <bugrovaoa@it2g.ru>
2020-10-17 21:56:45 +0400

    feat: add uncaughtException and unhandledRejection

--- src/app.js
+++ src/app.js
@@ -5,12 +5,13 @@ const YAML = require('yamljs');
 const userRouter = require('./resources/users/user.router');
 const boardRouter = require('./resources/boards/board.router');
 const taskRouter = require('./resources/tasks/task.router');
+const { INTERNAL_SERVER_ERROR, getStatusText } = require('http-status-codes');
+const morgan = require('morgan');
+const winston = require('./config/winston');
 const fs = require('fs');
 
 const app = express();
 const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
-
-const morgan = require('morgan');
 
 app.use(express.json());
 
@@ -23,19 +24,16 @@ morgan.token('body', res => {
   )}`;
 });
 
-app.use(morgan('METHOD: :method URL: :url :body :response-time :req[header]'));
-
 app.use(
-  morgan('METHOD: :method URL: :url :body :response-time :req[header]', {
-    stream: fs.createWriteStream(path.join(__dirname, './logs/access.log'), {
-      flags: 'a'
-    })
+  morgan('METHOD: :method URL: :url :body :response-time', {
+    stream: winston.stream
   })
 );
 
 app.use('/', (req, res, next) => {
   if (req.originalUrl === '/') {
     res.send('Service is running!');
+    winston.info('Required empty');
     return;
   }
   next();
@@ -44,5 +42,28 @@ app.use('/', (req, res, next) => {
 app.use('/users', userRouter);
 app.use('/boards', boardRouter);
 boardRouter.use('/:boardId/tasks', taskRouter);
+
+app.use((err, req, res, next) => {
+  winston.error(getStatusText(INTERNAL_SERVER_ERROR));
+  res.status(INTERNAL_SERVER_ERROR).send(getStatusText(INTERNAL_SERVER_ERROR));
+  next();
+});
+
+const globalState = {};
+
+process.on('uncaughtException', error => {
+  // eslint-disable-next-line no-sync
+  fs.writeFileSync(
+    `${__dirname}/logs/uncaughtException-%DATE%.log`,
+    JSON.stringify(globalState)
+  );
+  console.error(`captured error: ${error.message}`);
+  // eslint-disable-next-line no-process-exit
+  process.exit(1);
+});
+
+process.on('unhandledRejection', reason => {
+  console.error(`unhandled rejection detected: ${reason.message}`);
+});
 
 module.exports = app;
